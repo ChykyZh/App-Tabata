11f4f63a3822f358773e10223a612b6c227da67e
Eduardo <eduardocalvogracia@gmail.com>
2025-11-28 09:40:28 +0100

    contenedores

--- index.py
+++ index.py
@@ -1,11 +1,24 @@
-import sreamlit as st
+import streamlit as st
 
-st.title('Work Out Bitches!')
-
-c-progres = st.container()
-
-c-rutines = st.container(
-    rutin = st.container()
+st.set_page_config(
+    page_title='Workout!', 
+    page_icon="💪🏻",
+    layout="centered",
+    initial_sidebar_state='expanded'
 )
 
+#Titulo
+st.title('💪🏻 Workout!')
+st.divider()
 
+c_progres = st.container(border=True, width='stretch', horizontal_alignment='center')
+progres = st.container()
+
+c_rutines = st.container()
+rutines = st.container()
+
+with c_progres:
+    st.header("Progress")
+
+with c_rutines:
+    st.header('Rutinas')
